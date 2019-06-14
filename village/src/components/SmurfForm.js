395a0e1c88118e3ef693be88c744888136b59eb2
Michael Harris <mhguitar1997@gmail.com>
2019-06-14 10:31:08 -0700

    update form astyling

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import {Form, Input, Button, Label } from 'reactstrap';
+import React, { Component } from "react";
+import { Form, Input, Button, Label, FormGroup, Col } from "reactstrap";
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-    const {name, age, height} = this.state
+    const { name, age, height } = this.state;
     const newSmurf = {
       name: name,
       age: age,
-      height: height,
-    }
+      height: `${height}cm`
+    };
     this.props.addSmurf(newSmurf);
     this.setState({
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     });
-    this.props.history.push("/")
-  }
+    this.props.history.push("/");
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -37,31 +37,53 @@ class SmurfForm extends Component {
     return (
       <div className="SmurfForm">
         <Form onSubmit={this.addSmurf}>
-          <Label for="name">Name</Label>
-          <Input
-            onChange={this.handleInputChange}
-            placeholder="name"
-            value={this.state.name}
-            name="name"
-            id="name"
-          />
-          <Label for="age">Age</Label>
-          <Input
-            onChange={this.handleInputChange}
-            placeholder="age"
-            value={this.state.age}
-            name="age"
-            id="age"
-          />
-          <Label for="height">Height</Label>
-          <Input
-            onChange={this.handleInputChange}
-            placeholder="height"
-            value={this.state.height}
-            name="height"
-            id="height"
-          />
-          <Button type="submit">Add to the village</Button>
+          <FormGroup row>
+            <Label for="name" size="lg">
+              Name:
+            </Label>
+            <Col>
+              <Input
+                bsSize="lg"
+                onChange={this.handleInputChange}
+                placeholder="name"
+                value={this.state.name}
+                name="name"
+                id="name"
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="age" size="lg">
+              Age:
+            </Label>
+            <Col>
+              <Input
+                bsSize="lg"
+                onChange={this.handleInputChange}
+                placeholder="age"
+                value={this.state.age}
+                name="age"
+                id="age"
+                
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="height" size="lg">
+              Height:
+            </Label>
+            <Col>
+              <Input
+                bsSize="lg"
+                onChange={this.handleInputChange}
+                placeholder="height"
+                value={this.state.height}
+                name="height"
+                id="height"
+              />
+            </Col>
+          </FormGroup>
+          <Button type="submit" color="success">Add to the village</Button>
         </Form>
       </div>
     );
