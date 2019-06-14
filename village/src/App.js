749d2a6a9e98196f14a7f347a08b9f2fc5f79107
Michael Harris <mhguitar1997@gmail.com>
2019-06-14 08:35:35 -0700

    addSmurf function passed to form as a prop

--- village/src/App.js
+++ village/src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addSmurd={this.addSmurf}/>
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
